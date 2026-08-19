5d1dccc4fe3a41c6d6f920894514526af6f7c017
Flopster101 <nahuelgomez329@gmail.com>
2026-08-18 21:55:19 -0300

    sched: core: Remove `dbg_snapshot_task()` from __schedule
    Completely useless call just present there to cause overhead on every
    context switch for some debugging feature no one except Samsung's
    engineers will use.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4274,8 +4274,6 @@ static void __sched notrace __schedule(bool preempt)
 		rq->clock_update_flags &= ~(RQCF_ACT_SKIP|RQCF_REQ_SKIP);
 		rq_unlock_irq(rq, &rf);
 	}
-
-	dbg_snapshot_task(smp_processor_id(), rq->curr);
 	balance_callback(rq);
 }
 
